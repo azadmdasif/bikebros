75425fb9c42d7814df47cbe222c06c65b137dba1
azadmdasif <azadmdasif@gmail.com>
2025-12-02 18:01:57 +0530

    Remove App component and Google Analytics setup
    Removed the App component and its associated useEffect for Google Analytics.

--- bikebros_website/App.tsx
+++ bikebros_website/App.tsx
@@ -102,22 +102,5 @@ const App: React.FC = () => {
     </div>
   );
 };
-function App() {
-  useEffect(() => {
-    if (window.gtag) {
-      window.gtag("config", "G-TCSRH9SVJD", {
-        page_path: window.location.pathname,
-      });
-    }
-  }, []);
-
-  return (
-    <>
-      {/* your existing layout */}
-      <Hero />
-      {/* other sections: About, Rates, Contact, etc. */}
-    </>
-  );
-}
 
 export default App;
